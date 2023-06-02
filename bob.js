c860b1bc416b06ff06079529e07adc921e3b20f4
Heath Robbins <heathrobbins89@gmail.com>
2023-06-02 12:38:25 -0400

    Create bob.js

---
+++ bob.js
@@ -0,0 +1,15 @@
+var Bob = function() {};
+Bob.prototype.hey = function(input) {
+if (input.valueOf("?") != -1) {
+      console.log("Sure"); 
+   } else if {
+   if (input.valueOf("!") != -1) {
+      console.log("Whoa, chill out!");
+   } else if {
+   if (input.valueOf(" ") != -1) {
+      console.log("Fine. Be that way!");
+   } else {
+      console.log("Whatever");
+   }
+};
+module.exports = Bob;
